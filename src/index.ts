ee52ce79019dcd379893287195c46c91440b45e9
Jeff Tian <jie.tian@unicareer.com>
2018-11-01 15:31:47 +0800

    add {}

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-export const Greeter = (exe: string, exeFilePath: string, endpoint: string, protoFilePath: string) => `You started ${exeFilePath} by ${exe}, and it will start a gRPC client connecting to ${endpoint} and load the proto file from ${protoFilePath}`;
+export const Greeter = (exe: string, exeFilePath: string, endpoint: string, protoFilePath: string) => {
+  return `You started ${exeFilePath} by ${exe}, and it will start a gRPC client connecting to ${endpoint} and load the proto file from ${protoFilePath}`;
+};
 
 const [exe, exeFilePath, endpoint, protoFilePath] = process.argv;
+
 Greeter(exe, exeFilePath, endpoint, protoFilePath);
