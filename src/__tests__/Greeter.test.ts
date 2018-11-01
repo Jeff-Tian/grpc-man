956071ee9a0c7ac146075883b32b2481420274f9
Jeff Tian <jie.tian@unicareer.com>
2018-11-01 15:33:46 +0800

    1.0.1

--- src/__tests__/Greeter.test.ts
+++ src/__tests__/Greeter.test.ts
@@ -1,5 +1,7 @@
 import { Greeter } from '../Greeter';
 
 test('My Greeter', () => {
-  expect(Greeter('node', '.', 'localhost:8081', '.')).toBe('You started . by node, and it will start a gRPC client connecting to localhost:8081 and load the proto file from .');
+  expect(Greeter('node', '.', 'localhost:8081', '.')).toBe(
+    'You started . by node, and it will start a gRPC client connecting to localhost:8081 and load the proto file from .',
+  );
 });
