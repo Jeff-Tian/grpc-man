5470d3ebfbbb32e309bcea42a55167df350690b9
Jeff Tian <jie.tian@unicareer.com>
2018-11-08 21:05:02 +0800

    灵活的使用示例。

--- src/__tests__/Client.test.ts
+++ src/__tests__/Client.test.ts
@@ -3,7 +3,7 @@ import asyncCall from '../AsyncCall';
 
 require('./helper/mock-server');
 
-test('Client', async () => {
+test('Client version 1 (tobe deleted in the future)', async () => {
   expect(Client).toBeDefined();
 
   const client = Client.connect('0.0.0.0:8899', __dirname + '/./proto/helloworld.test.proto', 'helloworld.test', 'Greeter');
@@ -13,10 +13,16 @@ test('Client', async () => {
   const res = await asyncCall(client.sayHello, client)({ name: 'your name' });
   expect(res).toBeDefined();
   expect(res).toEqual({ 'message': 'Hello your name' });
+});
 
+test('Client version 2', async () => {
   const client2 = new Client('0.0.0.0:8899', __dirname + '/./proto/helloworld.test.proto');
   let service = client2.getService('helloworld.test.Greeter');
   expect(service).toBeDefined();
   expect(service.sayHello).toBeDefined();
+  expect(await asyncCall(service.sayHello, service)({ name: 'name' })).toEqual({ 'message': 'Hello name' });
+
+  const client = new Client('0.0.0.0:8899', __dirname + '/./proto/helloworld.proto');
+  service = client.getService('helloworld.Greeter');
   expect(await asyncCall(service.sayHello, service)({ name: 'name' })).toEqual({ 'message': 'Hello name' });
 });
--- src/__tests__/helper/mock-server.ts
+++ src/__tests__/helper/mock-server.ts
@@ -2,16 +2,21 @@ import * as grpc from 'grpc';
 import * as protoLoader from '@grpc/proto-loader';
 
 const PROTO_PATH = __dirname + '/../proto/helloworld.test.proto';
-const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+const PROTO_PATH2 = __dirname + '/../proto/helloworld.proto';
+let options = {
   keepCase: true,
   longs: String,
   enums: String,
   defaults: true,
   oneofs: true,
-});
+};
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, { ...options });
+const packageDefinition2 = protoLoader.loadSync(PROTO_PATH2, { ...options });
 
 let helloProto: any = grpc.loadPackageDefinition(packageDefinition).helloworld;
 helloProto = helloProto.test;
+
+let helloProto2: any = grpc.loadPackageDefinition(packageDefinition2).helloworld;
 
 function sayHello(call: any, callback: any) {
   callback(null, { message: 'Hello ' + call.request.name });
@@ -20,6 +25,7 @@ function sayHello(call: any, callback: any) {
 function main() {
   const server = new grpc.Server();
   server.addService(helloProto.Greeter.service, { sayHello: sayHello });
+  server.addService(helloProto2.Greeter.service, { sayHello: sayHello });
   server.bind('0.0.0.0:8899', grpc.ServerCredentials.createInsecure());
 
   server.start();
