269e690f230db94873481c1a8ec1df03941c07c7
Jeff Tian <jie.tian@unicareer.com>
2018-11-09 10:57:59 +0800

    1.1.0

--- src/__tests__/Client.test.ts
+++ src/__tests__/Client.test.ts
@@ -6,13 +6,18 @@ require('./helper/mock-server');
 test('Client version 1 (tobe deleted in the future)', async () => {
   expect(Client).toBeDefined();
 
-  const client = Client.connect('0.0.0.0:8899', __dirname + '/./proto/helloworld.test.proto', 'helloworld.test', 'Greeter');
+  const client = Client.connect(
+    '0.0.0.0:8899',
+    __dirname + '/./proto/helloworld.test.proto',
+    'helloworld.test',
+    'Greeter',
+  );
 
   expect(client).toBeDefined();
 
   const res = await asyncCall(client.sayHello, client)({ name: 'your name' });
   expect(res).toBeDefined();
-  expect(res).toEqual({ 'message': 'Hello your name' });
+  expect(res).toEqual({ message: 'Hello your name' });
 });
 
 test('Client version 2', async () => {
@@ -20,9 +25,9 @@ test('Client version 2', async () => {
   let service = client2.getService('helloworld.test.Greeter');
   expect(service).toBeDefined();
   expect(service.sayHello).toBeDefined();
-  expect(await asyncCall(service.sayHello, service)({ name: 'name' })).toEqual({ 'message': 'Hello name' });
+  expect(await asyncCall(service.sayHello, service)({ name: 'name' })).toEqual({ message: 'Hello name' });
 
   const client = new Client('0.0.0.0:8899', __dirname + '/./proto/helloworld.proto');
   service = client.getService('helloworld.Greeter');
-  expect(await asyncCall(service.sayHello, service)({ name: 'name' })).toEqual({ 'message': 'Hello name' });
+  expect(await asyncCall(service.sayHello, service)({ name: 'name' })).toEqual({ message: 'Hello name' });
 });
